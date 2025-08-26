f78ca2786cfb21a1b6e043acf66424d6c81896d7
MarinCervinschi <marincervinschi3@gmail.com>
2025-08-26 20:00:43 +0200

    feat(socket): ip simulation

--- Docker_UNIX/workspace/IP/ip.py
+++ Docker_UNIX/workspace/IP/ip.py
@@ -30,30 +30,28 @@ class IP:
     def get_net_id(address: str) -> Optional[str]:
         _class = IP.get_class(address)
         if _class not in ["A", "B", "C"]:
-            print(f"ERROR: NetID non previsto per la classe {_class}")
             return None
 
         b = address.split(".")
         match _class:
             case "A":
-                return ".".join([b[0], "0", "0", "0"])
+                return f"{b[0]}.0.0.0"
             case "B":
-                return ".".join([b[0], b[1], "0", "0"])
+                return f"{b[0]}.{b[1]}.0.0"
             case "C":
-                return ".".join([b[0], b[1], b[2], "0"])
+                return f"{b[0]}.{b[1]}.{b[2]}.0"
 
     @staticmethod
     def get_broadcast(address: str) -> Optional[str]:
         _class = IP.get_class(address)
         if _class not in ["A", "B", "C"]:
-            print(f"ERROR: Broadcast non previsto per la classe {_class}")
             return None
 
         b = address.split(".")
         match _class:
             case "A":
-                return ".".join([b[0], "255", "255", "255"])
+                return f"{b[0]}.255.255.255"
             case "B":
-                return ".".join([b[0], b[1], "255", "255"])
+                return f"{b[0]}.{b[1]}.255.255"
             case "C":
-                return ".".join([b[0], b[1], b[2], "255"])
+                return f"{b[0]}.{b[1]}.{b[2]}.255"
